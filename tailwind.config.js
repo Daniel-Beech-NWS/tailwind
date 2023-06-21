35941a3566dee1c7561197adc357e4c22f508bf8
Daniel <daniel.beech@networldsports.com>
2023-06-21 12:19:46 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/**/*.{html,js}"],
+  theme: {
+    extend: {      
+      colors: {
+      "element-base": "var(--default-color)",
+      nwsmain:{
+        DEFAULT: '#1D59D1'
+      },
+      nwshover:{
+        DEFAULT: '#0744BE'
+      },
+    },
+    fontFamily: { 
+      'sans': ['Helvetica', 'Arial', 'sans-serif']
+    },
+    screens: {
+      'hover-hover': {'raw': '(hover: hover)'},
+      'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+      'tallscreen': { 'raw': '(max-aspect-ratio: 13/20)' },
+},
+
+keyframes:{
+  'open-menu':{
+    '0%':{
+      transform: 'scaleY(0)',
+    },
+    '80%':{
+      transform: 'scaleY(1.2)',
+    },
+    '100%':{
+      transform: 'scaleY(1)',
+    }
+  }
+},
+animation:{
+  'open-menu': 'open-menu 0.5s ease-in-out forwards'
+}
+  },
+  },
+  plugins: [],
+}
+
